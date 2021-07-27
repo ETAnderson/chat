4e9857bd875ef08e08d76f02b3b8230240083323
Eric Anderson <anderson.eric.t@gmail.com>
2021-07-27 14:30:56 -0400

    extracted out the secretkey

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import project.routing
+from SECRETKEY import secretkey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5u1g_4!!)@q2b65yskzyt*l*7-f=uskat%3$@id1v+flbh&(0('
-
+SECRET_KEY = secretkey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
