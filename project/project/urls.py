445aeb73786ae3370c15c2a73a29f86215558545
Eric Anderson <anderson.eric.t@gmail.com>
2021-07-27 13:38:03 -0400

    fixed broken import in projects/urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
